9549f23c40db26af2e811a6e8aa3e098a6395cde
DnyaneshU <112575291+DnyaneshU@users.noreply.github.com>
2024-09-29 09:30:34 +0530

    Add files via upload

---
+++ Welcome First Program/Content/Full_name_Age_Gender.c
@@ -0,0 +1,8 @@
+// Print your full name, age and gender. 
+#include<stdio.h>
+#include<stdlib.h>
+int main() 
+{
+    printf("My name is: Dnyanesh Ujalambkar\nMy age is: 20\nMy gender is: Male\n");
+    return 0;
+}
---
+++ Welcome First Program/Content/Isoceles_Triangle.c
@@ -0,0 +1,8 @@
+// Print an isosceles triangle. 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    printf("  *\n ***\n*****\n");
+    return 0;
+}
