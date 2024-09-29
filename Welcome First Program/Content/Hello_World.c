7feb2fe4fe4aa3ed2d84b2f96eda201eede1adfb
DnyaneshU <112575291+DnyaneshU@users.noreply.github.com>
2024-09-29 09:32:27 +0530

    Delete Welcome First Program/Content directory

--- Welcome First Program/Content/Hello_World.c
+++
@@ -1,8 +0,0 @@
-// Write the first program that any programmer writes while starting to learn a new programming language.
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello World!");
-   return 0;
-}
