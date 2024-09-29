34b72cef1737a4c59a4701e0a32d6a2638143b5c
DnyaneshU <112575291+DnyaneshU@users.noreply.github.com>
2024-09-29 10:48:19 +0530

    Delete Welcome First Program/Content directory

--- Welcome First Program/Content/First_and_Last_name.c
+++
@@ -1,8 +0,0 @@
-// Print your first and last name.
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    printf("\nMy name is Dnyanesh Ujalambkar\n");
-    return 0;
-}
--- Welcome First Program/Content/Isoceles_Triangle.c
+++
@@ -1,8 +0,0 @@
-// Print an isosceles triangle. 
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    printf("  *\n ***\n*****\n");
-    return 0;
-}
